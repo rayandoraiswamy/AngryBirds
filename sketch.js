67c126e7c1fe12b3d1544b1d5e8f53ad332b1834
rayandoraiswamy <81537367+rayandoraiswamy@users.noreply.github.com>
2021-05-05 19:41:35 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint= Matter.Constraint;
 var engine, world;
 var box1, pig1;
-var backgroundImg;
+var backgroundImg,platform;
+var bird
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -16,8 +17,9 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
-    platform= new Ground(150,300,150,170)
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
+
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -32,8 +34,10 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    log6= new Log(230,180,80,PI/2)
     bird = new Bird(100,100);
+    Slingshot= new Slingshot(bird.body,log6.body)
+ 
 
 }
 
@@ -48,7 +52,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
-    platform.display();
+
     box3.display();
     box4.display();
     pig3.display();
@@ -57,6 +61,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    Slingshot.display();
+    log6.display();
     bird.display();
+    platform.display();
 }
